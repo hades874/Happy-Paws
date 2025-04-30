145e4813bf682bcdd5e848cff9c737c8f5ef1163
Hasan Farabi <45858413+hades874@users.noreply.github.com>
2025-05-01 01:50:41 +0600

    Add files via upload

---
+++ Controllers/Admin.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HappyPaws.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult addEmplyee()
+        {
+            return View();
+        }
+
+        public IActionResult petStay()
+        {
+            return View();
+        }
+
+        public IActionResult petAdopt()
+        {
+            return View();
+        }
+
+        public IActionResult vet()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HappyPaws.Controllers
+{
+    public class Employee : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
